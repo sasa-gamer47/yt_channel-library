681d732dbfd7b3b3a47be6932f29a34687098ef9
sasa-gamer47 <giacomofilippi237@gmail.com>
2022-07-10 16:08:30 +0200

    starting

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
+}
+
+export default Home
